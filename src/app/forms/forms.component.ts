0ff932a9ed0b89203d3009620a8c05a3635033f2
Aaron O'Brien <aaron.obrien62@gmail.com>
2019-07-16 06:31:00 -0400

    forms component update to call suppliers

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {Suppliers} from '../model/supplier-details';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css']
 })
-export class FormsComponent implements OnInit {
+export class FormsComponent {
+  companies: Suppliers[];
+  company = this.companies[0];
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private http: HttpClient) {
+
+    this.http.get<Suppliers[]>('assets/data/Suppliers.json')
+      .subscribe(company => this.companies = company);
   }
-
 }
