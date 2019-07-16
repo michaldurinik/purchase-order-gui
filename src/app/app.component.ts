d88878ca28d359651ccc64e08b45cbb1b4bafa72
Joao Colososchi <colososchi@gmail.com>
2019-07-16 05:32:21 -0400

    login page css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
   title = 'LIT Purchase Order';
-=======
->>>>>>> master
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
-
-=======
 import { LoginComponent } from './login/login.component';
 import { FormsComponent } from './forms/forms.component';
 import { OrdersComponent } from './orders/orders.component';
 import {RouterModule} from '@angular/router';
 import {appRoutes} from './services/routes';
 import { HomeComponent } from './home/home.component';
->>>>>>> master
 
 @NgModule({
   declarations: [
@@ -26,13 +22,10 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     ButtonModule,
-<<<<<<< HEAD
     InputTextModule,
     PasswordModule,
-=======
     RouterModule,
     RouterModule.forRoot(appRoutes)
->>>>>>> master
   ],
   providers: [],
   bootstrap: [AppComponent]
