052dacf4f6fe2c126a265218d088b5fd20a8f4d8
Aaron O'Brien <aaron.obrien62@gmail.com>
2019-07-19 08:55:52 -0400

    updates to travel form added routing and most up to date version of master

--- src/app/services/routes.ts
+++ src/app/services/routes.ts
@@ -3,6 +3,7 @@ import {LoginComponent} from '../login/login.component';
 import {FormsComponent} from '../forms/forms.component';
 import {OrdersComponent} from '../orders/orders.component';
 import {HomeComponent} from '../home/home.component';
+import {TravelFormComponent} from '../travel-form/travel-form.component';
 
 export const appRoutes: Routes = [
   { path: '', component: LoginComponent },
@@ -10,5 +11,6 @@ export const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'forms', component: FormsComponent },
   { path: 'orders', component: OrdersComponent },
+  { path: 'travel-form', component: TravelFormComponent },
   { path: '**', redirectTo: ''},
 ];
