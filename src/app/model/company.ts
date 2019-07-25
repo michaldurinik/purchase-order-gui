67f880b10b05aaa04a53c8d4990b2308008ae9a3
Michal Durinik <michal.durinik2@mail.dcu.ie>
2019-07-25 11:35:44 +0100

    Delete company.ts

--- src/app/model/company.ts
+++
@@ -1,6 +0,0 @@
-export class Company {
-  constructor( public companyName: string,
-               public companyAddress: string,
-               public companyPhone: string,
-               public companyEmail: string) {}
-}
