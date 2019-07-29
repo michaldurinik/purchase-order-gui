2d40d7141877004a583cfaca334dcc4d8b71776e
Danny Tuite <danny.a.tuite@gmail.com>
2019-07-29 09:07:14 -0500

    Form default component completed with lists

--- src/app/default-form/default-form.component.ts
+++ src/app/default-form/default-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { NavbarService } from '../services/navbar.service';
 
 @Component({
@@ -14,76 +14,57 @@ export class DefaultFormComponent implements OnInit {
   defaultForm: FormGroup;
   isSubmitted = false;
   public show = false;
-
   ngOnInit() {
     this.navbarService.setTitle(this.title);
     this.defaultForm = this.formBuilder.group({
-      purchaseOrderNo: ['', Validators.required],
-      purchaseOrderDate: ['', Validators.required],
-      requesterNnumber: ['', Validators.required],
-      requesterName: ['', Validators.required],
-      purchaseOrderLocation: ['', Validators.required],
+      Currency: ['', Validators.required],
+      order: this.formBuilder.group({
+          purchaseOrderNo: ['', Validators.required],
+          purchaseOrderDate: ['', Validators.required],
+          requesterNnumber: ['', Validators.required],
+          requesterName: ['', Validators.required],
+          purchaseOrderLocation: ['', Validators.required],
+          authorisedBy: ['', Validators.required],
+          unitName: ['', Validators.required],
+          nameSDL: ['', Validators.required]
+        }),
       // company nested group
       company: this.formBuilder.group({
-      companyName: ['', Validators.required],
-      companyAddress: ['', Validators.required],
-      companyPhoneNumber: ['', Validators.required],
-      companyEmail: ['', Validators.required]
+          companyName: ['', Validators.required],
+          companyAddress: ['', Validators.required],
+          companyPhoneNumber: ['', Validators.required],
+          companyEmail: ['', Validators.required],
       }),
       // end nested group
       // Each Traveller has a hotel & flight and car of their own nested group
-      traveller: this.formBuilder.group({
-        travellerName: ['', Validators.required],
-        travellerNnumber: ['', Validators.required],
-        hotel: this.formBuilder.group({
-          hotelName: ['', Validators.required],
-          hotelCheckIn: ['', Validators.required],
-          hotelCheckOut: ['', Validators.required],
-          hotelCost: ['', Validators.required],
-          hotelAccountUse: ['', Validators.required]
-        }),
-        flight: this.formBuilder.group( {
-          flightAirline: ['', Validators.required],
-          departureAirport: ['', Validators.required],
-          arrivalAirport: ['', Validators.required],
-          flightCheckIn: ['', Validators.required],
-          flightCheckOut: ['', Validators.required],
-          flightCost: ['', Validators.required],
-          flightAccountUse: ['', Validators.required]
-        }),
-        car: this.formBuilder.group({
-          addCarName: ['', Validators.required],
-          addCarCollectionDate: ['', Validators.required],
-          addCarReturnDate: ['', Validators.required],
-          addCarPricePerDay: ['', Validators.required]
-        })
-      }),
+      traveller: this.formBuilder.array([this.buildTravellers()]),
       // end of group
       // purchase order nested group
-      item: this.formBuilder.group({
-        itemQuantity: ['', Validators.required],
-        itemDescription: ['', Validators.required],
-        Currency: ['', Validators.required],
-        itemPricePerUnit: ['', Validators.required]
-      }),
+      item: this.formBuilder.array([this.buildItems()]),
       // end group
       // course nested group
-      course: this.formBuilder.group({
-      nameCourse: ['', Validators.required],
-      dateCourseStart: ['', Validators.required],
-      courseDuration: ['', Validators.required],
-      courseLocation: ['', Validators.required],
-      directorName: ['', Validators.required],
-      coursePrice: ['', Validators.required],
-      priceIncludeHotel: ['', Validators.required]
-      }),
+      course: this.formBuilder.array([this.buildCourses()]),
       // end group
-      authorisedBy: ['', Validators.required],
-      unitName: ['', Validators.required],
-      nameSDL: ['', Validators.required]
     });
   }
-
+  addItem(): void {
+    this.item.push(this.buildItems());
+  }
+  addCourse(): void {
+    this.course.push(this.buildCourses());
+  }
+  addTraveller(): void {
+    this.traveller.push(this.buildTravellers());
+  }
+  get course(): FormArray {
+    return this.defaultForm.get('course') as FormArray;
+  }
+  get item(): FormArray {
+    return this.defaultForm.get('item') as FormArray;
+  }
+  get traveller(): FormArray {
+    return this.defaultForm.get('traveller') as FormArray;
+  }
   get formControls() { return this.defaultForm.controls; }
   toggle() {
     this.show = !this.show;
@@ -91,4 +72,52 @@ export class DefaultFormComponent implements OnInit {
   update() {
     console.log(this.defaultForm.value);
   }
+  buildTravellers() {
+    return this.formBuilder.group({
+      travellerName: ['', Validators.required],
+      travellerNnumber: ['', Validators.required],
+      hotel: this.formBuilder.group({
+        hotelName: ['', Validators.required],
+        hotelCheckIn: ['', Validators.required],
+        hotelCheckOut: ['', Validators.required],
+        hotelCost: ['', Validators.required],
+        hotelAccountUse: ['', Validators.required]
+      }),
+      flight: this.formBuilder.group( {
+        flightAirline: ['', Validators.required],
+        departureAirport: ['', Validators.required],
+        arrivalAirport: ['', Validators.required],
+        flightCheckIn: ['', Validators.required],
+        flightCheckOut: ['', Validators.required],
+        flightCost: ['', Validators.required],
+        flightAccountUse: ['', Validators.required]
+      }),
+      car: this.formBuilder.group({
+        addCarName: ['', Validators.required],
+        addCarCollectionDate: ['', Validators.required],
+        addCarReturnDate: ['', Validators.required],
+        addCarPricePerDay: ['', Validators.required]
+      })
+    });
+  }
+  buildCourses() {
+    return this.formBuilder.group({
+      nameCourse: ['', Validators.required],
+      dateCourseStart: ['', Validators.required],
+      courseDuration: ['', Validators.required],
+      courseLocation: ['', Validators.required],
+      directorName: ['', Validators.required],
+      coursePrice: ['', Validators.required],
+      amountAttending: ['', Validators.required],
+      priceIncludeHotel: [false, Validators.required]
+    });
+  }
+  buildItems() {
+    return this.formBuilder.group({
+      itemQuantity: ['', Validators.required],
+      itemDescription: ['', Validators.required],
+      itemPricePerUnit: ['', Validators.required]
+    });
+  }
 }
+
